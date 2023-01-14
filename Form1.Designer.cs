34c44bc4ada4e2b0ea813a498699b972ca3994ea
Jachou <84678307+Jachou-yt@users.noreply.github.com>
2023-01-14 23:07:49 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace JavaVersionManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.parameters = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // parameters
+            // 
+            this.parameters.BackColor = System.Drawing.Color.Transparent;
+            this.parameters.Location = new System.Drawing.Point(12, 871);
+            this.parameters.Name = "parameters";
+            this.parameters.Size = new System.Drawing.Size(131, 36);
+            this.parameters.TabIndex = 0;
+            this.parameters.Text = "Paramètres";
+            this.parameters.UseVisualStyleBackColor = false;
+            this.parameters.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "JavaVersionManager";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1709, 919);
+            this.Controls.Add(this.parameters);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "JavaVersionManager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button parameters;
+    }
+}
+
