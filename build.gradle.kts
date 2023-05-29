96387b20b96063dfc5a94502d2fbe56408a7cc47
Jachou <matisse.martayan@gmail.com>
2023-05-29 12:02:37 +0200

    Chnage name to lower case for publishing

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Jachou-yt/JavaVersionManager")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+group = "fr.jachou"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("fr.flowarg:flowmultitools:1.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/fr/jachou/jvm/managers/JavaVersionManager.java
@@ -0,0 +1,148 @@
+package fr.jachou.jvm.managers;
+
+import fr.flowarg.flowzipper.ZipUtils;
+import fr.jachou.jvm.Main;
+import fr.jachou.jvm.managers.utils.JavaVersionList;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JavaVersionManager {
+
+    private String downloadURL;
+    private JavaVersionList list;
+    private Path path;
+
+    /**
+     * JavaVersionManager constructor
+     * @param customUrl The custom url to download the java version on your own server
+     */
+    public JavaVersionManager(String customUrl) {
+        this.downloadURL = customUrl;
+    }
+
+    /**
+     * JavaVersionManager constructor
+     */
+
+    public JavaVersionManager() {
+        this.downloadURL = "https://chiss.fr/jvm/download/";
+    }
+
+    /**
+     * Download a java version
+     * @param version The java version to download
+     * @param path The path to download the java version
+     */
+
+    public void downloadVersion(JavaVersionList version, Path path) {
+        this.path = path;
+        this.list = version;
+        try {
+            URL url = new URL(this.downloadURL + version + ".zip");
+
+            Path downloadPath = Paths.get(version + ".zip");
+            Files.copy(url.openStream(), downloadPath);
+
+            Main.getLogger().info("Downloaded " + version + " to " + path.toAbsolutePath().toString() + " successfully!");
+        } catch (IOException e) {
+            Main.getLogger().err("Failed to download " + version + " to " + path.toAbsolutePath().toString() + "!");
+        }
+    }
+
+    /**
+     * Download a java version
+     * @param version The java version to download
+     * @param path The path to download the java version
+     */
+
+    public void downloadVersion(JavaVersionList version, String path) {
+        this.path = Paths.get(path);
+        this.list = version;
+        try {
+            URL url = new URL(this.downloadURL + version + ".zip");
+            Path downloadPath = this.path.resolve(version + ".zip");
+            Files.copy(url.openStream(), downloadPath);
+
+
+            Main.getLogger().info("Downloaded " + version + " to " + path + " successfully!");
+        } catch (IOException e) {
+            e.getMessage();
+            Main.getLogger().err("Failed to download " + version + " to " + path + "!");
+        }
+    }
+
+    /**
+     * Unzip a java version
+     */
+
+    public void unzipJavaVersion() {
+        if (this.path == null) {
+            Main.getLogger().err("Failed to unzip java version because the path is null!");
+            return;
+        }
+        if (this.list == null) {
+            Main.getLogger().err("Failed to unzip java version because the list is null!");
+            return;
+        }
+        try {
+            ZipUtils.unzip(this.path, Paths.get(this.path + this.list.toString() +".zip"));
+        } catch (IOException e) {
+            Main.getLogger().err("Failed to unzip java version " + this.list + "!");
+        }
+    }
+
+    /**
+     * Unzip a java version
+     * @param path The path where the java zip version is downloaded
+     * @param list The java version list
+     */
+
+    public void unzipJavaVersion(Path path, JavaVersionList list) {
+        try {
+            Main.getLogger().info(list.toString());
+            ZipUtils.unzip(path, Paths.get(path + "\\" + list.toString() +".zip"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Main.getLogger().err("Failed to unzip java version " + list + "!");
+        }
+    }
+
+    /**
+     * Get the download url of the java version
+     * @return The download url of the java version
+     */
+    public String getDownloadURL() {
+        return downloadURL;
+    }
+
+    /**
+     * Set the download url of the java version
+     * @param downloadURL The download url of the java version
+     */
+    public void setDownloadURL(String downloadURL) {
+        this.downloadURL = downloadURL;
+    }
+
+    /**
+     * Get the java version list
+     * @return The java version list
+     */
+
+    public JavaVersionList getList() {
+        return list;
+    }
+
+    /**
+     * Set the java version list
+     * @param list The java version list
+     */
+
+    public void setList(JavaVersionList list) {
+        this.list = list;
+    }
+}
